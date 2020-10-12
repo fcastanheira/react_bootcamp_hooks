08939a4c6532fa6538e58f363e83321f7ec73523
c-santogon <goncalo.santos@diconium.com>
2020-10-12 14:21:16 +0100

    first scratch of the demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleInputTextChange = this.handleInputTextChange.bind(this);
+        this.handleAddTodo = this.handleAddTodo.bind(this);
+        this.handleDeleteTodo = this.handleDeleteTodo.bind(this);
+        this.handleCompleteTodo = this.handleCompleteTodo.bind(this);
+    }
+
+    handleInputTextChange() {
+        //TODO handle the changes on the text input
+    };
+
+    handleAddTodo() {
+        //TODO add a todo to the TodoList
+    };
+
+    handleDeleteTodo() {
+        //TODO delete a todo from the TodoList
+    };
+
+    handleCompleteTodo() {
+        //TODO mark a todo as completed
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <div className="header">
+                    MyTodo list
+                </div>
+                <div className="overview-wrapper">
+                    <div className="list-wrapper">
+                        <div className="list">
+                            <div className="list-item">
+                                <div className="item">
+                                    <input type="checkbox" onChange={this.handleCompleteTodo}/>
+                                    <div className="item-name completed"> Prepare my presentation </div>
+                                </div>
+                                <button
+                                    className="delete-button"
+                                    type="button"
+                                    onClick={this.handleDeleteTodo}
+                                >
+                                    X
+                                </button>
+                            </div>
+                            <div className="list-item">
+                                <div className="item">
+                                    <input type="checkbox" onChange={this.handleCompleteTodo}/>
+                                    <div className="item-name"> Do my presentation </div>
+                                </div>
+                                <button
+                                    className="delete-button"
+                                    type="button"
+                                    onClick={this.handleDeleteTodo}
+                                >
+                                    X
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form">
+                        <button className="input-button" type="button" onClick={this.handleAddTodo}>
+                            Add item
+                        </button>
+                        <input
+                            className="input-text"
+                            type="text"
+                            name={'text'}
+                            onChange={this.handleInputTextChange}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
