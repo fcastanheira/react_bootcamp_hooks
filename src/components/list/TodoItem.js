c2689122b187eb0240796fae15408841990a7e18
c-santogon <goncalo.santos@diconium.com>
2020-10-14 22:52:53 +0100

    - step-0 completed

--- src/components/list/TodoItem.js
+++ src/components/list/TodoItem.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const TodoItem = ({ onCompleteTodo, onRemoveTodo, todo, }) => {
+class TodoItem extends Component {
 
-    const {
-        id = '',
-        name = '',
-        completed = false,
-    } = todo;
+    render() {
+        const {
+            onCompleteTodo,
+            onRemoveTodo,
+            todo,
+        } = this.props;
 
-    return (
-        <div className="list-item" >
-            <div className="item">
-                <input
-                    type="checkbox"
-                    onChange={() => onCompleteTodo(id)}
-                    checked={completed}
-                />
-                <div className={
-                    `item-name ${completed ? 'completed' : ''}`
-                }>
-                    {name}
+        const {
+            id = '',
+            name = '',
+            completed = false,
+        } = todo;
+
+        return (
+            <div className="list-item" >
+                <div className="item">
+                    <input
+                        type="checkbox"
+                        onChange={() => onCompleteTodo(id)}
+                        checked={completed}
+                    />
+                    <div className={
+                        `item-name ${completed ? 'completed' : ''}`
+                    }>
+                        {name}
+                    </div>
                 </div>
+                <button
+                    className="delete-button"
+                    type="button"
+                    onClick={() => onRemoveTodo(id)}
+                >
+                    X
+                </button>
             </div>
-            <button
-                className="delete-button"
-                type="button"
-                onClick={() => onRemoveTodo(id)}
-            >
-                X
-            </button>
-        </div>
-    );
+        );
+    };
 }
 
 TodoItem.propTypes = {
